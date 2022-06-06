33c2c61a37ac9243be7758daf8b347d4d7a721be
Voy19 <denys.voytov@gmail.com>
2022-06-06 17:33:15 +0300

    test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React please!
+          Learn Reac please!
         </a>
       </header>
     </div>
